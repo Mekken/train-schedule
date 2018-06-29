6ceb77fa32871b814be2f92d71d00b197ce9a4b3
Donnie Heard <dhearddev@gmail.com>
2018-06-28 23:51:20 -0700

    making time dynamic and only update based on user's time

--- js/trainschedule.js
+++ js/trainschedule.js
@@ -8,20 +8,20 @@ $(document).ready(function() {
     var database = firebase.initializeApp(config).database();
 
     $('#submit').on('click',function() {
-        //TODO: Add Error Handling for empty Strings, all values should be required!
         var formattedTime = moment($('#train-time').val().trim(),'HH:mm').format('hh:mm A');
-        console.log(formattedTime);
         
         var nameVal  = $('#trainName').val().trim(),
         destinationVal  = $('#destination').val().trim(),
         trainTimeVal  = formattedTime,
-        frequencyVal = $('#frequency').val().trim()
+        frequencyVal = $('#frequency').val().trim(),
+        nextArrival = moment(trainTimeVal,'hh:mm A').add(frequencyVal,'m')
 
         database.ref().push({
             name: nameVal,
             destinationName: destinationVal,
             trainTime: trainTimeVal,
-            frequencyTime: frequencyVal
+            frequencyTime: frequencyVal,
+            arrivalTime:  nextArrival.unix()
         })
 
         $('#trainName').val('');
@@ -32,17 +32,24 @@ $(document).ready(function() {
 
     database.ref().on('child_added', function(snapShot) {
         var sv = snapShot.val();
-        var nextArrival = undefined;
+        var arrivalTime = moment.unix(sv.arrivalTime);
         var minutesAway = undefined;
+
+        console.log("Next Arrival Time: " + arrivalTime.format('hh:mm A'));
         
-        nextArrival = moment().add(sv.frequencyTime,'m')
-        minutesAway = nextArrival.diff(moment(),'m');
+        while(arrivalTime.diff(moment(),'m') <= 0) 
+        {
+            console.log('adding time...');
+            arrivalTime.add(sv.frequencyTime,'m');
+        }
+
+        minutesAway = arrivalTime.diff(moment(),'m');
 
         console.log("SnapShot Name: " + sv.name);
         console.log("SnapShot Destination: " + sv.destinationName);
         console.log("SnapShot Train Time: " + sv.trainTime);
         console.log("SnapShot Frequency: " + sv.frequencyTime);
-        console.log("Next Arrival Time: " + nextArrival.format('hh:mm A'));
+        console.log("Next Arrival Time: " + arrivalTime.format('hh:mm A'));
         console.log("Minutes Away: " + minutesAway);
 
         var tbodyElem = $('#train-records');
@@ -50,7 +57,7 @@ $(document).ready(function() {
         var tdName = $('<td>').addClass('train-name').append(sv.name);
         var tdDestination = $('<td>').addClass('destination').append(sv.destinationName);
         var tdFrequency = $('<td>').addClass('frequency').append(sv.frequencyTime);
-        var tdNextArrival = $('<td>').addClass('next-arrival').append(nextArrival.format('hh:mm A'));
+        var tdNextArrival = $('<td>').addClass('next-arrival').append(arrivalTime.format('hh:mm A'));
         var tdMinutesAway = $('<td>').addClass('minutes-away').append(minutesAway);
 
         trElem.append(tdName);
